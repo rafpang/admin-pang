2631165cac49a6be8bf23c8b433c7caa244b2fa4
Rafi Pangestu <rafibintangpagi@gmail.com>
2023-12-19 12:53:16 +0700

    add api/v1 to api url settings

--- app/settings.ts
+++ app/settings.ts
@@ -1,3 +1,3 @@
 export const REGISTRATION_DUE: string = "2023-12-21";
-export const API_URL: string = "https://api.icnmusical.com"; // (rafpang) change once backend deployed
+export const API_URL: string = "https://api.icnmusical.com/api/v1"; // (rafpang) change once backend deployed
 export const COOKIE_EXPIRY_MINUTES: number = 180;
