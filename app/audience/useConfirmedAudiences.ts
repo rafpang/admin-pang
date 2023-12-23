4275f39c025f2a80a1afdceeeb3982c230df03a2
Rafi Pangestu <rafibintangpagi@gmail.com>
2023-12-23 18:04:40 +0700

    added loading state to signal data processing

--- app/audience/useConfirmedAudiences.ts
+++ app/audience/useConfirmedAudiences.ts
@@ -2,16 +2,19 @@ import { useEffect, useState } from "react";
 import { useAuthorizedInitialFetch } from "@/app/hooks/fetch";
 
 export default function useConfirmedAudiences() {
-    const [filteredAudienceData, setFilteredAudienceData] = useState([]);
+    const [filteredAudienceData, setFilteredAudienceData] = useState<any[]>([]);
     const [isLoadingAudience, audienceData] = useAuthorizedInitialFetch(
         "https://api.icnmusical.com/api/v1/audiences/protected"
     );
+    const [isDataProcessingLoading, setIsDataProcessingLoading] =
+        useState<boolean>(false);
 
     const [isLoadingOrders, ordersData] = useAuthorizedInitialFetch(
         "https://api.icnmusical.com/api/v1/orders/protected"
     );
 
     useEffect(() => {
+        setIsDataProcessingLoading(true);
         const filteredData = audienceData.filter((audience: any) => {
             return ordersData.some((order: any) => {
                 return (
@@ -21,10 +24,12 @@ export default function useConfirmedAudiences() {
             });
         });
         setFilteredAudienceData(filteredData);
+        setIsDataProcessingLoading(false);
     }, [audienceData, ordersData]);
 
     return {
-        isLoading: isLoadingAudience || isLoadingOrders,
+        isLoading:
+            isLoadingAudience || isLoadingOrders || isDataProcessingLoading,
         filteredAudienceData,
     };
 }
