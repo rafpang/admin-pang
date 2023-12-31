4864f8239c5ce783a46f54e9c5e148277372d46c
Rafi Pangestu <rafibintangpagi@gmail.com>
2024-01-01 02:15:47 +0700

    use useMemo for confirmed audiences

--- app/audience/useConfirmedAudiences.ts
+++ app/audience/useConfirmedAudiences.ts
@@ -1,35 +1,32 @@
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 import { useAuthorizedInitialFetch } from "@/app/hooks/fetch";
 
+function findConfirmedAudiences(audienceData: any, ordersData: any) {
+    const filteredData = audienceData.filter((audience: any) => {
+        return ordersData.some((order: any) => {
+            return (
+                order.orderId === audience.orderId &&
+                order.paymentStatus === "successful"
+            );
+        });
+    });
+    return filteredData;
+}
+
 export default function useConfirmedAudiences() {
-    const [filteredAudienceData, setFilteredAudienceData] = useState<any[]>([]);
     const [isLoadingAudience, audienceData] = useAuthorizedInitialFetch(
         "https://api.icnmusical.com/api/v1/audiences/protected"
     );
-    const [isDataProcessingLoading, setIsDataProcessingLoading] =
-        useState<boolean>(false);
-
     const [isLoadingOrders, ordersData] = useAuthorizedInitialFetch(
         "https://api.icnmusical.com/api/v1/orders/protected"
     );
-
-    useEffect(() => {
-        setIsDataProcessingLoading(true);
-        const filteredData = audienceData.filter((audience: any) => {
-            return ordersData.some((order: any) => {
-                return (
-                    order.orderId === audience.orderId &&
-                    order.paymentStatus === "successful"
-                );
-            });
-        });
-        setFilteredAudienceData(filteredData);
-        setIsDataProcessingLoading(false);
-    }, [audienceData, ordersData]);
+    const filteredAudienceData = useMemo(
+        () => findConfirmedAudiences(audienceData, ordersData),
+        [audienceData, ordersData]
+    );
 
     return {
-        isLoading:
-            isLoadingAudience || isLoadingOrders || isDataProcessingLoading,
+        isLoading: isLoadingAudience || isLoadingOrders,
         filteredAudienceData,
     };
 }
