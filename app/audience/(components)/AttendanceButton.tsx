7473aebd0a8fc95ac5a14cf509abb59497f089fd
Rafi Pangestu <rafibintangpagi@gmail.com>
2024-01-01 21:25:37 +0700

    removed react memo import from attendance button

--- app/audience/(components)/AttendanceButton.tsx
+++ app/audience/(components)/AttendanceButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState, memo } from "react";
+import React, { useState } from "react";
 import { Button } from "@mui/material";
 import { API_URL } from "@/app/settings";
 import { useAttendanceContext } from "@/app/(contexts)/AttendanceContext";
